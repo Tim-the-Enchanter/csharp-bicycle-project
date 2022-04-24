391e1085b7a860dfaca2eb3ae29cc6db7f7fda46
Tim Donnelley <sgtpixel@gmail.com>
2022-04-23 21:17:44 -0500

    CAC-103 Prep For sale, Inventoryand Sample order/receipt

--- CSharpBicycleProject/Interfaces/IBicycle.cs
+++ CSharpBicycleProject/Interfaces/IBicycle.cs
@@ -3,13 +3,18 @@
 internal interface IBicycle
 {
     IWheelInterface Wheel { get; }
-    BikeColor FrameColor {  get; set; }
+    BikeColor FrameColor { get; set; }
     decimal Price { get; }
     string Model { get; }
     string Style { get; }
+    string SerialNumber { get; set; }
     int StartNumber { get; }
 
     void Paint(BikeColor color);
 
+    // Prepare for sale
+    void CleanFrame();
+    void AirTires();
+    void TestRide();
 
-} // End Interface
+} // end interface
